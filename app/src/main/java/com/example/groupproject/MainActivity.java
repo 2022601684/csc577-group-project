9eaa2a1ce0e3731f5063a515a7a9e0cb1a8cf087
2022601684 <20220601684@student.uitm.edu.my>
2025-01-14 11:12:42 +0800

    my changes

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -20,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //The gameplay code will be here
     }
 }
